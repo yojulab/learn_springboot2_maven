b63fa0c7d9ed1eca0dc1f7d393919866010c674f
sanghun oh <sanghunoh@sanghuns-MacBook-Air.local>
2019-07-28 14:28:17 +0900

    add HomeController

---
+++ src/main/java/com/example/learn_springboot/controller/HomeController.java
@@ -0,0 +1,32 @@
+package com.example.learn_springboot.controller;
+
+import java.sql.Date;
+import java.text.DateFormat;
+import java.util.Locale;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/home", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		Date date = new Date(0);
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+
+		String formattedDate = dateFormat.format(date);
+
+		model.addAttribute("serverTime", formattedDate);
+
+		return "home";
+	}
+}
