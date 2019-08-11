22f760b45902759d894e4b909287642171b714e7
sanghun oh <sanghunoh@sanghuns-MacBook-Air.local>
2019-08-11 17:41:25 +0900

    add repository with mybatis

--- src/main/java/com/example/learn_springboot/controller/IndexController.java
+++ src/main/java/com/example/learn_springboot/controller/IndexController.java
@@ -8,12 +8,18 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
+import com.example.learn_springboot.repository.ShareDao;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Handles requests for the application home page.
@@ -52,4 +58,40 @@ public class IndexController {
 		return "index";
 	}
 
+	@Autowired
+	public ShareDao service;
+
+	@RequestMapping(value = "/member/{action}", method = { RequestMethod.GET, RequestMethod.POST })
+	public ModelAndView actionMethod(@RequestParam Map<String, Object> paramMap, @PathVariable String action,
+			ModelAndView modelandView) {
+
+		String viewName = "/member/" + action ;
+
+		Map<String, Object> resultMap = new HashMap<String, Object>() ;
+		List<Object> resultList = new ArrayList<Object>();
+
+		// divided depending on action value
+		if ("edit".equalsIgnoreCase(action)) {
+		} else if ("update".equalsIgnoreCase(action)) {
+			resultMap = (Map<String, Object>) service.getObject(paramMap);
+			paramMap.put("action", action);
+		} else if ("merge".equalsIgnoreCase(action)) {
+			resultMap = (Map<String, Object>) service.saveObject(paramMap);
+		} else if ("read".equalsIgnoreCase(action)) {
+			resultMap = (Map<String, Object>) service.getObject(paramMap);
+		} else if ("list".equalsIgnoreCase(action)) {
+			resultList = (List<Object>) service.getList(paramMap);
+		} else if ("list_pagination".equalsIgnoreCase(action)) {
+			resultMap = (Map<String, Object>) service.getListPagination(paramMap);
+		} else if ("delete".equalsIgnoreCase(action)) {
+			resultList = (List<Object>) service.deleteObject(paramMap);
+		} 
+		
+		modelandView.setViewName(viewName);
+
+		modelandView.addObject("paramMap", paramMap);
+		modelandView.addObject("resultMap", resultMap);
+		modelandView.addObject("resultList", resultList);
+		return modelandView;
+	}
 }
