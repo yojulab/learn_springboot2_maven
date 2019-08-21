9848a3aa8289ddf0dd8e0d89313db5f0576e94f4
sanghun oh <sanghunoh@sanghuns-MacBook-Air.local>
2019-08-21 10:27:10 +0900

    add thymeleaf sample

--- src/main/java/com/example/learn_springboot/controller/ByPassController.java
+++ src/main/java/com/example/learn_springboot/controller/ByPassController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class ByPassController {
-	@RequestMapping(value = {"/home", "/login", "/indexlayout"})
+	@RequestMapping(value = {"/home", "/login", "/exampleLayout"})
 	public void byPass() {
 	}
 }
